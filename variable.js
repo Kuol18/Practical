5991a1b464e5a50509d1d954db0a7eee618e369d
Kuol18 <93642034+Kuol18@users.noreply.github.com>
2022-01-27 11:45:18 +0300

    ✨ new javascript practice

---
+++ variable.js
@@ -0,0 +1,52 @@
+// this is variable 1
+let number1
+
+// this is variable 2
+let number2 =200
+
+// this is my results
+let results
+
+//initialization
+number1 = 100
+number2 = 300
+
+//operation
+result = number1 + number2
+
+console.log(result)
+
+//calculate the product of two number in javascript
+
+//initialization
+num1 = 100
+num2 = 100
+
+//operation
+result = num1 * num2
+
+console.log(result)
+
+//calculate the average of five students
+
+//initialization
+let Student1 = 78
+let Student2 = 76
+let Student3 = 74
+let Student4 = 72
+let Student5 = 70
+
+//operation
+let Total = Student1 + Student2 + Student3 + Student4 + Student5
+
+let Average = Total/5
+
+//this line of code prints the first line of input
+console.log("Hello, World".);
+
+// write the second line of output that prints the contents of 'inputString' here
+inputString = "Welcome to 30 Days of code";
+return inputString;
+}
+console.log(processData())
+
